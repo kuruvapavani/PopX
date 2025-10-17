17bd7ee7137e342f4fe2c298382b6604e93feb3f
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-10-17 19:49:23 +0530

    Add layout component and initial page structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import UserProfile from './pages/UserProfile';
 const App = () => {
   return (
-    <div className='min-h-screen bg-black text-white'>Hii</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/profile' element={<UserProfile />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+const Welcome = () => {
+  return (
+    <Layout>
+      <h1>Hii</h1>
+    </Layout>
+  )
+}
+
+export default Welcome
